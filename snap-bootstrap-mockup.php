ed83bc80fba7ac081499980823fe71ae52e78c8a
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-07 08:23:44 -0600

    Added style tag and viewport tag

--- snap-bootstrap-mockup.php
+++ snap-bootstrap-mockup.php
@@ -1,6 +1,23 @@
-<?php
-/**
- * Create a page using the preceding mockups
- * using only Bootstrap defaults and NO CSS
- */
+<!--Create a page using the preceding mockups using only Bootstrap defaults and NO CSS-->
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>ST: TNG</title>
+      <!--CSS Styling-->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+   </head>
+   <body>
 
+
+
+
+
+
+
+
+
+
+   </body>
+</html>
