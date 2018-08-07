6f086bda4d7e86ba9469a41f38eb5597b3c1823d
Half Mortise <HalfMortise@users.noreply.github.com>
2018-08-07 08:36:56 -0600

    Added navbar

--- snap-bootstrap-mockup.php
+++ snap-bootstrap-mockup.php
@@ -5,11 +5,38 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>ST: TNG</title>
-      <!--CSS Styling-->
+      <!--CSS Styling: Bootstrap 4 only-->
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+
    </head>
    <body>
-
+      <nav class="navbar navbar-expand-lg navbar-light bg-blue">
+         <a href="#"><img width="50px" src="clipart.jpg" alt=""/></a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+               <li class="nav-item active">
+                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+               </li>
+               <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                     Dropdown
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                     <a class="dropdown-item" href="#">Action</a>
+                     <a class="dropdown-item" href="#">Another action</a>
+                     <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+               </li>
+            </ul>
+         </div>
+      </nav>
 
 
 
